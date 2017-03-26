ad836d0812efdec0234d87f2408848a6f33bb256
meghanroserebecca <meghanroserebecca@gmail.com>
2017-03-25 22:38:25 -0700

    working to get server up and running

---
+++ test/user.routes.test.js
@@ -0,0 +1,67 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+chai.use(chaiHttp);
+
+const assert = chai.assert;
+
+const app = require('../lib/app');
+const mongoose = require('mongoose');
+
+process.env.DB_URI = 'mongodb://localhost:27017/user-routes-test';
+require('../lib/connection');
+
+describe('user', () => {
+    before(() => mongoose.connection.dropDatabase());
+
+    const user = {
+        username: 'user',
+        password: 'password'
+    }
+
+    const request = chai.request(app);
+
+    describe('user management', () => {
+        
+        const badRequest = (url, data, error) =>
+            request
+                /post(url)
+                .send(data)
+                .then(
+                    () => { throw new Error('Status should not be OK'); },
+                    res => {
+                        assert.equal(res.status, 400)
+                        assert.equal(res.response.body.error, error);
+                    }
+                );
+        it('signup requires username', () =>
+            badRequest('/signup', { password: 'password' },
+            'username and passowrd must be provided')
+        );
+
+        it('signup requires password', () => 
+            badRequest('/signup', { username: 'username' }, 'username and password must be provided')
+        );
+
+        let token = '',
+
+        it('signup', () => 
+            request
+                .post('/signup')
+                .send(user)
+                .then(res => assert.ok(token = res.body.token))
+        );
+
+        it('can\'t use same username', () => 
+            request
+                .post('signup')
+                .send(user)
+                .then(
+                    () => { throw new Error('status should not be ok'); },
+                    res => {
+                        assert.equal(res.status, 400);
+                        assert.equal(res.response.body.error, 'username user already exists')
+                    }
+                )
+        );
+    })
+})
